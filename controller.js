4eb990bdc1114b39d2ca8b2be733a75447cde8cf
IndraHardika <motorantapani@gmail.com>
2024-01-31 12:40:28 +0700

    dsdssdw

---
+++ controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./config/database');
+
+
+exports.index = function(req, res) {
+    response.ok('rest api jalan bloww',res)
+};
+
